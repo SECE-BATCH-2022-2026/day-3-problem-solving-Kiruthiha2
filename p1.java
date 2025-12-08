7ae5793278541ce56f2ed27d6da00afcabe93359
Kiruthihashree <kiruthihashree.s2022cse@sece.ac.in>
2025-12-08 15:37:13 +0530

    second commit

--- p1.java
+++ p1.java
@@ -2,7 +2,7 @@
 import java.io.*;
 import java.util.*;
 
-public class CountOccurence{
+public class p1{
 	public static void main(String args[]){
 	Scanner sc=new Scanner(System.in);
 	String s1=sc.nextLine();
